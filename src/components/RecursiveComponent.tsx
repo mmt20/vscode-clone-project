284ee85103d242939c9c1d4fd0469202f9fc5c3f
mmt20 <mmt202002@gmail.com>
2025-02-07 11:05:10 +0200

    feat: Implement folder open/close state in RecursiveComponent

--- src/components/RecursiveComponent.tsx
+++ src/components/RecursiveComponent.tsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import { IFile } from "../interfaces";
 import FileIcon from "./SVG/File";
 import FolderIcon from "./SVG/Folder";
+import RightArrowIcon from "./SVG/Right";
+import BottomArrowIcon from "./SVG/Bottom";
 
 interface IProps {
   fileTree: IFile;
 }
 
-const RecursiveComponent = ({ fileTree }: IProps) => {
+const RecursiveComponent = ({
+  fileTree: { name, isFolder, children },
+}: IProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  // ** Handlers
+  const Toggle = () => setIsOpen((prev) => !prev);
   return (
-    <div className="mb-2 ml-2 ">
+    <div className="mb-2 ml-2 cursor-pointer">
       <div className="flex items-center mb-1">
-        <span className="mr-2">
-          {fileTree.isFolder ? <FolderIcon /> : <FileIcon />}
-        </span>
-        <span>{fileTree.name}</span>
+        {isFolder ? (
+          <div className="flex items-center" onClick={Toggle}>
+            {isOpen ? <BottomArrowIcon /> : <RightArrowIcon />}
+            <FolderIcon />
+            <span>{name}</span>
+          </div>
+        ) : (
+          <div className="flex items-center mr-2">
+            <FileIcon />
+            <span className="ml-2">{name}</span>
+          </div>
+        )}
       </div>
-      {fileTree.children &&
-        fileTree.children.map((file, idx) => (
+      {isOpen &&
+        children &&
+        children.map((file, idx) => (
           <RecursiveComponent fileTree={file} key={idx} />
         ))}
     </div>
